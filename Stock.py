f2e75a73701ec87065822ec5f67669e00f87a14e
Akshay kathuria <akshay.kathuria118@gmail.com>
2024-04-04 14:23:39 -0700

    update

--- Stock.py
+++ Stock.py
@@ -6,19 +6,25 @@ import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
+from keras.models import Sequential
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Dropout
+
 df = pd.read_csv('D:/Data Science/Projects/Current_project/stock_data.csv')
 training_set = df.iloc[:,1:2].values
 scaler = MinMaxScaler(feature_range = (0,1))
 scaled_training_set = scaler.fit_transform(training_set)
+
 x_train = []
 y_train = []
-for i in range(1,0):
-    x_train.append(scaled_training_set[i-60:i, 0])
+
+for i in range(len(scaled_training_set)):
+    x_train.append(scaled_training_set[i])
     y_train.append(scaled_training_set[i,0])
-X_train = np.array(x_train)
+    
+x_train = np.array(x_train)
 y_train = np.array(y_train)
-print(x_train.shape)
-print(y_train.shape)
-# x_train = np.array(x_train)
-# x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-# print(x_train.shape)
+
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
