fe807c61b0e9002344199ea69068658c4721fefa
Akshay kathuria <akshay.kathuria118@gmail.com>
2024-04-05 15:08:50 -0700

    Graph is still not right

--- Stock.py
+++ Stock.py
@@ -7,63 +7,60 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
 from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
-from keras.layers import Dropout
+from keras.layers import LSTM, Dense, Dropout
 
+# Read the dataset
 df = pd.read_csv('D:/Data Science/Projects/Current_project/stock_data.csv')
-training_set = df.iloc[:,1:2].values
-scaler = MinMaxScaler(feature_range = (0,1))
+training_set = df.iloc[:, 1:2].values
+
+# Scale the training set
+scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_training_set = scaler.fit_transform(training_set)
 
+# Prepare training data for LSTM
 x_train = []
 y_train = []
-
 for i in range(len(scaled_training_set)):
     x_train.append(scaled_training_set[i])
-    y_train.append(scaled_training_set[i,0])
-    
+    y_train.append(scaled_training_set[i, 0])
 x_train = np.array(x_train)
 y_train = np.array(y_train)
-
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
+# Define and train the LSTM model
 regressor = Sequential()
-
-regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+regressor.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
 regressor.add(Dropout(0.2))
-
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(LSTM(units=50, return_sequences=True))
 regressor.add(Dropout(0.2))
-
-regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(LSTM(units=50, return_sequences=True))
 regressor.add(Dropout(0.2))
-
-regressor.add(LSTM(units = 50))
+regressor.add(LSTM(units=50))
 regressor.add(Dropout(0.2))
-
 regressor.add(Dense(units=1))
-regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-regressor.fit(x_train, y_train, epochs=100, batch_size=32)
+regressor.compile(optimizer='adam', loss='mean_squared_error')
+regressor.fit(x_train, y_train, epochs=1, batch_size=32)
 
-dataset_total = pd.concat((df[open], df[open]), axis = 0)
-inputs = dataset_total[len(dataset_total) -len(df)-60:].values
-
-inputs = inputs.reshape(-1,1)
-inputs = scaler.tranform(inputs)
-
+# Prepare test data for prediction
+dataset_total = pd.concat((df['open'], df['open']), axis=0)
+inputs = dataset_total[len(dataset_total) - len(df) - 60:].values
+inputs = inputs.reshape(-1, 1)
+inputs = scaler.transform(inputs)
 x_test = []
-for i in range (60,90):
-    x_test.append(inputs[i-60:i, 0])
+for i in range(60, 90):
+    x_test.append(inputs[i - 60:i, 0])
 x_test = np.array(x_test)
-x_test = np.reshape(x_test,(x_test.shape[0], x_test.shape[1], 1))
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
+# Predict stock prices
 predicted_stock_price = regressor.predict(x_test)
 predicted_stock_price = scaler.inverse_transform(predicted_stock_price)
 
-plt.plot(training_set, color = 'red', label = 'Current Stock Price')
-plt.plot(predicted_stock_price, color = 'blue', label = 'Predicted Stock Price')
+# Plot the results
+plt.plot(range(len(training_set)), training_set, color='red', label='Current Stock Price')
+plt.plot(range(len(training_set), len(training_set) + len(predicted_stock_price)), predicted_stock_price, color='blue', label='Predicted Stock Price')
 plt.title('Stock Price Prediction By Akshay Kathuria')
 plt.xlabel('Time')
 plt.ylabel('Price')
 plt.legend()
+plt.show()
