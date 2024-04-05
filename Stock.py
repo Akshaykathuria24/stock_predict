6bf858696cb5c170aff9451eddc7aa27b918b7e3
Akshay kathuria <akshay.kathuria118@gmail.com>
2024-04-04 20:59:12 -0700

    update

--- Stock.py
+++ Stock.py
@@ -28,3 +28,20 @@ y_train = np.array(y_train)
 
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
+regressor = Sequential()
+
+regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], 1)))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50, return_sequences = True))
+regressor.add(Dropout(0.2))
+
+regressor.add(LSTM(units = 50))
+regressor.add(Dropout(0.2))
+
+regressor.add(Dense(units=1))
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+regressor.fit(x_train, y_train, epochs=100, batch_size=32)
